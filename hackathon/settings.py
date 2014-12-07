7192df0dcd87d99e60b11eff5786907e4a0224dc
davgon35 <davgon35@gmail.com>
2014-12-07 00:09:27 -0600

    terminando los modelos

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'planetofheroes',
 
     # 'allauth.socialaccount.providers.amazon',
     # 'allauth.socialaccount.providers.angellist',
