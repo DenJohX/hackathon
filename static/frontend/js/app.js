7257f9b8190260e5e35c16a92a2cbd7209cba929
Dennis Xiloj <denjohx@gmail.com>
2014-12-08 01:33:56 -0600

    fix map width

--- static/frontend/js/app.js
+++ static/frontend/js/app.js
@@ -4,16 +4,13 @@ $(document).foundation();
 
 $( "#Poverty" ).click(function(e) {
     e.preventDefault();
-  $("#title").text("World poverty gap $1.25/day");
   GetData("Poverty");
 });
 $( "#Education" ).click(function(e) {
     e.preventDefault();
-  $("#title").text("Lower secondary completion rate");
   GetData("Education");
 });
 $( "#Health" ).click(function(e) {
     e.preventDefault();
-  $("#title").text("Number of neonatal deaths");
   GetData("Health");
 });
--- static/frontend/js/map.js
+++ static/frontend/js/map.js
@@ -5,6 +5,7 @@
 function GetData(data_type) {
     var gdpData={};
     var scale_type = 0;
+    var normalize_function = 'lineal';
     var ParseJsonP = function(data)
     {
         gdpData = {};
@@ -28,10 +29,10 @@ function GetData(data_type) {
                 regions: [{
                     values: gdpData,
                     scale: scale,
-                    normalizeFunction: 'exponential'
+                    normalizeFunction: normalize_function
                 }]
             },
-            backgroundColor: '#80DEEA',
+            backgroundColor: 'transparent',
             onRegionTipShow: function(e, el, code){
                 el.html(el.html()+' ('+gdpData[code]+')');
             }
@@ -43,21 +44,27 @@ function GetData(data_type) {
     //Number of neonatal deaths
     if(data_type=="Health"){
         scale_type = 0;
-        $("#title").text("Number of neonatal deaths");
+        normalize_function = 'polinomial';
+        $("#title").text("Number of neonatal deaths 2012-2014");
         url ="http://api.worldbank.org/countries/indicators/SH.DTH.NMRT?per_page=3800&date=2012:2014&format=jsonP&prefix=getdata";
     }
     //Lower secondary completion rate, total EDUCATION
     else if(data_type=="Education"){
+        normalize_function = 'lineal';
         scale_type = 1;
         url ="http://api.worldbank.org/countries/indicators/SE.SEC.CMPT.LO.ZS?per_page=3000&date=2005:2014&format=jsonP&prefix=getdata";
-        $("#title").text("Lower secondary completion rate");
+        $("#title").text("Lower secondary completion raten 2005 - 2014");
     }
     //pobreza per capita $1.25/dia poverty
     else if(data_type=="Poverty"){
+        normalize_function = 'logarithmic';
         scale_type = 0;
-        url = 'http://api.worldbank.org/countries/indicators/SI.POV.GAPS?per_page=10000&date=2000:2014&format=jsonP&prefix=getdata';
-        $("#title").text("World poverty gap $1.25/day");
+        url = 'http://api.worldbank.org/countries/indicators/SI.POV.GAPS?per_page=10000&date=2008:2014&format=jsonP&prefix=getdata';
+        $("#title").text("World poverty gap $1.25/day 2008 - 2014");
     }
+
+    $('#world-map').empty().html('Loading...');
+
     $.ajax({
         url : url,
         dataType:"jsonp",
