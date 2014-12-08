339488e7d18d220d7a66e5c58d4ae1acc7663834
Dennis Xiloj <denjohx@gmail.com>
2014-12-07 21:56:28 -0600

    deploy inicial koding

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -108,6 +108,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
+
 ROOT_URLCONF = 'hackathon.urls'
 
 WSGI_APPLICATION = 'hackathon.wsgi.application'
